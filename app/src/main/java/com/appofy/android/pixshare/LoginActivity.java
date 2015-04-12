9ba9d391d70f5e4c9abfb0b795a8d0e8037c1a8d
Rohan <rohan.tan@gmail.com>
2015-04-11 20:13:35 -0700

    Facebook Login integrated

--- app/src/main/java/com/appofy/android/pixshare/LoginActivity.java
+++ app/src/main/java/com/appofy/android/pixshare/LoginActivity.java
@@ -1,19 +1,121 @@
 package com.appofy.android.pixshare;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.ProfilePictureView;
+import com.facebook.share.widget.ShareDialog;
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends FragmentActivity {
+
+    private ProfilePictureView profilePictureView;
+    private TextView greeting;
+    private CallbackManager callbackManager;
+    private ShareDialog shareDialog;
+    private ProfileTracker profileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        updateUI();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        updateUI();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        updateUI();
+                    }
+
+                });
+
         setContentView(R.layout.activity_login);
+        profileTracker = new ProfileTracker() {
+
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                updateUI();
+            }
+
+        };
+
+        profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
+        greeting = (TextView) findViewById(R.id.greeting);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Call the 'activateApp' method to log an app event for use in analytics and advertising
+        // reporting.  Do so in the onResume methods of the primary Activities that an app may be
+        // launched into.
+        AppEventsLogger.activateApp(this);
+        updateUI();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Call the 'deactivateApp' method to log an app event for use in analytics and advertising
+        // reporting.  Do so in the onPause methods of the primary Activities that an app may be
+        // launched into.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void updateUI() {
+        boolean enableButtons = AccessToken.getCurrentAccessToken() != null;
+        Profile profile = Profile.getCurrentProfile();
+        if (enableButtons && profile != null) {
+            profilePictureView.setProfileId(profile.getId());
+            greeting.setText(getString(R.string.hello_user, profile.getFirstName()));
+        } else {
+            profilePictureView.setProfileId(null);
+            greeting.setText(null);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
