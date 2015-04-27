a69d688334bac22342fe8a320d449c1e48b304b5
m2linkin <asthana.mihir@gmail.com>
2015-04-26 23:48:05 -0700

    Added Groups and Friends Activities

--- app/src/main/java/com/appofy/android/pixshare/ManageFriendsActivity.java
+++ app/src/main/java/com/appofy/android/pixshare/ManageFriendsActivity.java
@@ -2,16 +2,73 @@ package com.appofy.android.pixshare;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class ManageFriendsActivity extends ActionBarActivity {
 
+    ListView lv;
+    EditText inputSearch;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_friends);
+        lv =(ListView)findViewById(R.id.friend_list_view);
+
+        inputSearch = (EditText)findViewById(R.id.inputFriendSearch);
+
+        String[] values = new String[] { "Friend 1", "Friend 2", "Friend 3" };
+        adapter = new ArrayAdapter<String>(this,
+
+                R.layout.friend_list_item,R.id.friend_name, values);
+
+        lv.setAdapter(adapter);
+
+        /**
+         * Enabling Search Filter
+         * */
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        /**
+         * Enabling onCLickListener Filter
+         * */
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3) {
+                String value = (String) adapter.getItemAtPosition(position);
+                Toast.makeText(getBaseContext(), value, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
--- app/src/main/java/com/appofy/android/pixshare/ManageGroupsActivity.java
+++ app/src/main/java/com/appofy/android/pixshare/ManageGroupsActivity.java
@@ -2,16 +2,73 @@ package com.appofy.android.pixshare;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class ManageGroupsActivity extends ActionBarActivity {
 
+    ListView lv;
+    EditText inputSearch;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_groups);
+        lv =(ListView)findViewById(R.id.group_list_view);
+
+        inputSearch = (EditText)findViewById(R.id.inputGroupSearch);
+
+        String[] values = new String[] { "Group 1", "Group 2", "Group 3" };
+        adapter = new ArrayAdapter<String>(this,
+
+                R.layout.group_list_item,R.id.group_name, values);
+
+        lv.setAdapter(adapter);
+
+        /**
+         * Enabling Search Filter
+         * */
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        /**
+         * Enabling onCLickListener Filter
+         * */
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3) {
+                String value = (String) adapter.getItemAtPosition(position);
+                Toast.makeText(getBaseContext(), value, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
