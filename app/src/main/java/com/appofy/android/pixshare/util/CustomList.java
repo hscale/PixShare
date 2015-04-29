0db4d5740a2acbfbe9058f13ba2ea8def6e84953
Rohan <rohan.tan@gmail.com>
2015-04-28 19:18:50 -0700

    friend and group activities added

---
+++ app/src/main/java/com/appofy/android/pixshare/util/CustomList.java
@@ -0,0 +1,7 @@
+package com.appofy.android.pixshare.util;
+
+/**
+ * Created by user on 27-04-2015.
+ */
+public class CustomList {
+}
---
+++ app/src/main/java/com/appofy/android/pixshare/util/CustomListGroups.java
@@ -0,0 +1,7 @@
+package com.appofy.android.pixshare.util;
+
+/**
+ * Created by user on 28-04-2015.
+ */
+public class CustomListGroups {
+}
